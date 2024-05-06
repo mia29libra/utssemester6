856793924e4e547b3aee4cb4c91d95bdd322739d
mia29libra <mia29libra@gmail.com>
2024-05-06 15:50:48 +0700

    model laprankeuangan

--- PKP/pemasukan/models.py
+++ PKP/pemasukan/models.py
@@ -18,3 +18,12 @@ class Pengeluaran(models.Model):
 
     def __str__(self):
         return self.tgl_Pengeluaran
+    
+class laporankeuangan(models.Model):
+    total_pemasukan = models.CharField(max_length=100)
+    total_pengeluaran= models.CharField(max_length=10, unique=True)
+    saldo_akhir = models.CharField(max_length=10, unique=True)
+    
+
+    def __str__(self):
+        return self.total_pemasukan
